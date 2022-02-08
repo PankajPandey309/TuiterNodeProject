332f3d61b0cc7ce4dbaad0dca73fb94a584856a7
Pankaj Pandey <pankajpandey@Pankajs-MacBook-Pro.local>
2022-02-08 15:20:42 -0500

    commit 1

--- server.ts
+++ server.ts
@@ -1,11 +1,24 @@
 import express, {Request, Response} from 'express';
-const app = express();
+import mongoose from "mongoose";
+mongoose.connect('mongodb+srv://Pankaj:secretpassword@cluster0.anx3x.mongodb.net/tuiter?retryWrites=true&w=majority');
+import bodyParser from "body-parser";
 
-app.get('/hello', (req: Request, res: Response) =>
-    res.send('Hello World!'));
+import UserController from "./controllers/UserController";
+import TuitController from "./controllers/TuitController";
+
+const app = express();
+app.use(bodyParser.json())
+
+app.get('/', (req: Request, res: Response) =>
+    res.send('Welcome!'));
 
 app.get('/add/:a/:b', (req: Request, res: Response) =>
     res.send(req.params.a + req.params.b));
+
+
+const userController = UserController.getInstance(app);
+const tuitController = TuitController.getInstance(app);
+
 
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
