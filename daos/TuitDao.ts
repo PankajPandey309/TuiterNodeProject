37b865e4d289b9ac601e6863508944e522d90776
Pankaj Pandey <pankajpandey@Pankajs-MacBook-Pro.local>
2022-02-09 01:17:58 -0500

    commit1

--- daos/TuitDao.ts
+++ daos/TuitDao.ts
@@ -1,6 +1,6 @@
 import TuitModel from "../mongoose/TuitModel";
 import Tuit from "../models/Tuit";
-import TuitDaoI from "../interfaces/TuitDaoI";
+import TuitDaoI from "../../interfaces/TuitDaoI";
 export default class TuitDao implements TuitDaoI{
     private static tuitDao: TuitDao | null = null;
     public static getInstance = (): TuitDao => {
